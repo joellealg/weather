3959ebcd2df721f3659a3bfd05a5f4fdb4c07b5a
[joellealg] <[joelleal@ufl.edu]>
2016-09-16 11:22:48 -0400

    Change to use yesterday's date for weather

--- weather.sh
+++ weather.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-curl "https://www.wunderground.com/history/airport/GNV/2016/09/06/DailyHistory.heml?&format=1" > gnv.txt
+echo "What day do you want to know?"
+
+read day
+
+echo "What month do you want to know?"
+
+read month
+
+echo "What year do you want to know?"
+
+read year
+
+curl "https://www.wunderground.com/history/airport/GNV/$year/$month/$day/DailyHistory.heml?&format=1" > gnv.txt
 
 maxTemp=`awk -F',' '{print $2}' gnv.txt | sort -n | tail -n1`
 
